585a87e4db1f874b57f8761ddedb6f656dc802b9
izaura <izaura.domi@gmail.com>
2021-02-02 21:48:13 +0100

    changes in .env

--- car_rental/.config.py
+++ car_rental/.config.py
@@ -1,8 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 DB = {
-    "host": "localhost",
-    "port": 3306,
-    "user": "root",
-    "password": "rootroot",
-    "database": "car_rental",
-    "charset": "utf8mb4",
+    "host": os.getenv("DATABASE_HOST"),
+    "port": int(os.getenv("DATABASE_PORT", "3306")),
+    "user": os.getenv("DATABASE_USER"),
+    "password": os.getenv("DATABASE_PASSWORD"),
+    "database": os.getenv("DATABASE_NAME"),
+    "charset": os.getenv("DATABASE_CHARSET"),
 }
+
+DB_URI = os.getenv("DATABASE_URI")
+
+APP_ENV = os.getenv("APP_ENV")
--- car_rental/api/cars.py
+++ car_rental/api/cars.py
@@ -2,4 +2,4 @@ from sqlalchemy import create_engine
 
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-import .config
+
