003a6ecf89061fe13f6180c57e080b0ef1044769
izaura <izaura.domi@gmail.com>
2021-02-03 23:29:26 +0100

    class Client

--- car_rental/db.py
+++ car_rental/db.py
@@ -15,17 +15,19 @@ class Client(Base):
     __tablename__ = "client"
 
     id=Column(Integer, primary_key=True, autoincrement=True)
-    customer_name=Column(String(30), nullable=False)
+    first_name=Column(String(30), nullable=False)
+    last_name=Column(String(30), nullable=False)
+    license_number=Column(String(20),nullable=False)
     address=Column(String(100), nullable=False)
     phone=Column(String(10),nullable=False)
     email=Column(String(15), nullable=True)
     login_at = Column(TIMESTAMP, default=datetime.datetime.utcnow)
     loout_at = Column(TIMESTAMP, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
-#kto dy rreshtat lart i kam vene gjasme per te treguar kur ehste loguar dhe cloguar klienti
+
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.id} {self.first_name_name} {self.last_name}"
 
     def __repr__(self):
-        return f"Person(id={self.id}, first_name={self.first_name!r})"
+        return f"Client(id={self.id}, first_name={self.first_name!r}, last_name={self.last_name})"
 
 
