4e04563c357f58a446d691ffe454e24556f0e4bc
izaura <izaura.domi@gmail.com>
2021-02-03 22:57:27 +0100

    changes in .env class Client

--- car_rental/db.py
+++ car_rental/db.py
@@ -11,3 +11,21 @@ engine = create_engine(DB_URI, echo=True)
 Base = declarative_base(bind=engine)
 Session = sessionmaker(bind=engine)
 
+class Client(Base):
+    __tablename__ = "client"
+
+    id=Column(Integer, primary_key=True, autoincrement=True)
+    customer_name=Column(String(30), nullable=False)
+    address=Column(String(100), nullable=False)
+    phone=Column(String(10),nullable=False)
+    email=Column(String(15), nullable=True)
+    login_at = Column(TIMESTAMP, default=datetime.datetime.utcnow)
+    loout_at = Column(TIMESTAMP, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __repr__(self):
+        return f"Person(id={self.id}, first_name={self.first_name!r})"
+
+
